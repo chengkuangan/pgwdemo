9e3a36ecd85bc675e056577e8e70fc59fadb6c59
CK GAN <chgan@redhat.com>
2021-05-24 15:02:41 +0800

    Removed Unit Test

--- sc/KafkaTopicViewer/src/test/java/blog/braindose/paygate/kafka/NativeCreditViewerIT.java
+++
@@ -1,9 +0,0 @@
-package blog.braindose.paygate.kafka;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCreditViewerIT extends CreditViewerTest {
-
-    // Execute the same tests but in native mode.
-}
